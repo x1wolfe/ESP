5a9980fe4a7cbb78f6c3908722b7bb34e91200e6
Arcadian420 <kaclhhdo@gmail.com>
2023-08-22 15:22:12 +0200

    update

--- esp.lua
+++ esp.lua
@@ -1,1 +1,18 @@
-
+function AddESP(part,TextColor,BillBoardName)
+        
+        local bill = Instance.new("BillboardGui", part)
+        bill.Name = BillBoardName
+        bill.Size = UDim2.new(30,50,30,50)
+        bill.Adornee = part
+        bill.AlwaysOnTop = true
+        local name = Instance.new("TextLabel", bill)
+         name.Font = Enum.Font.Arial
+        name.TextWrapped = true
+        name.Text = part.Name
+        name.Size = UDim2.new(1,0,1,0)
+        name.TextYAlignment = "Center"
+        name.TextColor3 = TextColor
+        name.BackgroundTransparency = 1
+        name.TextSize = 11
+        name.TextStrokeTransparency = 0.5
+    end
