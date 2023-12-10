a6284c260a3ad891fc9fc36df1c4428afcdc2cec
Arcadian420 <kaclhhdo@gmail.com>
2023-12-10 17:37:26 +0100

    fixing

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
+--Booleans
+getgenv().PlayerEsp = false
+
+--Functions
 function AddPartESP(part,TextColor,BillBoardName)
-        
         local bill = Instance.new("BillboardGui", part)
         bill.Name = BillBoardName
         bill.Size = UDim2.new(30,50,30,50)
@@ -12,42 +15,38 @@ function AddPartESP(part,TextColor,BillBoardName)
         name.TextYAlignment = "Center"
         name.TextColor3 = TextColor
         name.BackgroundTransparency = 1
-        name.TextSize = 11
-        name.TextStrokeTransparency = 0.5
+        name.TextSize = 12
+        name.TextStrokeTransparency = 0
 
         RunService.RenderStepped:Connect(function()
-name.Text = part.Parent.Name.." ["..GetDistance(Character.HumanoidRootPart,part).."]"
+name.Text = part.Name.." ["..GetDistance(Character.HumanoidRootPart,part).."]"
     end)
 end
 
-    function AddPlayerESP(part,TextColor,BillBoardName)
+function AddPlayerEsp(char,TargetName,TextColor,BillBoardName)
+    local Target = char:FindFirstChild(TargetName)
 
-    if LocalPlayer.Name == part.Parent.Name then
+    local bill = Instance.new("BillboardGui", Target)
+    bill.Name = BillBoardName
+    bill.Size = UDim2.new(30,50,30,50)
+    bill.Adornee = Target
+    bill.AlwaysOnTop = true
+    local name = Instance.new("TextLabel", bill)
+     name.Font = Enum.Font.Arial
+    name.TextWrapped = true
+    name.Size = UDim2.new(1,0,1,0)
+    name.TextYAlignment = "Center"
+    name.TextColor3 = TextColor
+    name.BackgroundTransparency = 1
+    name.TextSize = 12
+    name.TextStrokeTransparency = 0
 
-        else
-        
-        local bill = Instance.new("BillboardGui", part)
-        bill.Name = BillBoardName
-        bill.Size = UDim2.new(30,50,30,50)
-        bill.Adornee = part
-        bill.AlwaysOnTop = true
-        local name = Instance.new("TextLabel", bill)
-         name.Font = Enum.Font.Arial
-        name.TextWrapped = true
-        name.Size = UDim2.new(1,0,1,0)
-        name.TextYAlignment = "Center"
-        name.TextColor3 = TextColor
-        name.BackgroundTransparency = 1
-        name.TextSize = 11
-        name.TextStrokeTransparency = 0.5
-
-        RunService.RenderStepped:Connect(function()
-            if Character:FindFirstChild("Head") and part.Parent then
-                name.Text = part.Parent.Name.." ["..GetDistance(Character.HumanoidRootPart,part).."]"
+    RunService.RenderStepped:Connect(function()
+if getgenv().PlayerEsp == true then
+name.Text = char.Name.." ["..GetDistance(Character.HumanoidRootPart,Target).."]"
+bill.Enabled = true
 else
-    end
-    end)
-    end
-    end
-
-    --Example AddPlayerESP(character.Head,Color3.new(0,255,255),"PlayerESP")
+bill.Enabled = false
+end
+end)
+end
