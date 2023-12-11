407effb8e4f437e6e6aeb0cf3dfbbb058249e58d
Arcadian420 <kaclhhdo@gmail.com>
2023-12-11 17:59:01 +0100

    ads

--- main.lua
+++ main.lua
@@ -1,27 +1,4 @@
 --Functions
-function AddPartESP(part,TextColor,BillBoardName)
-        local bill = Instance.new("BillboardGui", part)
-        bill.Name = BillBoardName
-        bill.Size = UDim2.new(30,50,30,50)
-        bill.Adornee = part
-        bill.AlwaysOnTop = true
-        local name = Instance.new("TextLabel", bill)
-         name.Font = Enum.Font.Arial
-        name.TextWrapped = true
-        name.Size = UDim2.new(1,0,1,0)
-        name.TextYAlignment = "Center"
-        name.TextColor3 = TextColor
-        name.BackgroundTransparency = 1
-        name.TextSize = 12
-        name.TextStrokeTransparency = 0
-
-        RunService.RenderStepped:Connect(function()
-            if bill.Enabled == true then
-name.Text = part.Name.." ["..GetDistance(Character.HumanoidRootPart,part).."]"
-    end
-    end)
-end
-
 function AddPlayerEsp(char,TargetName,TextColor,BillBoardName)
     local Target = char:FindFirstChild(TargetName)
 
